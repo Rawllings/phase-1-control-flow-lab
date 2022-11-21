eadd410aee3c300015787a342500fad0c5e86e47
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-21 21:59:54 +0300

    phase 1 Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+let distance = 199;
+
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+    return 'This one is on me!';
+  }
+  else if (distance <= 2500 ){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500){ 
+    return 'No can do.';
+  }
+}
+let city ; 
+
+function ternaryCheckCity(city) {
+  // Write your code here!
+const ternaryCheckCity = city  ?  "Ok, sounds good." : "No go.";
+return ternaryCheckCity;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+const tip = "generous";
+
+let comments;
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch (tip) {
+  case "generous": 
+  comments = 'Thank you so much.';
+   break;
+
+  case "not as generous": 
+  comments = 'Thank you.';
+   break;
+
+  default:
+ comments = 'Bye.';
+    break;
+
+}
+return comments;
 }
